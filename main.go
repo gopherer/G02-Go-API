3d0d6ebdf45489b93d9ea4c851c8783889f51e32
gopherer <wuhe092@gmail.com>
2023-05-30 14:02:54 +0800

    初始化路由

--- main.go
+++ main.go
@@ -1,42 +1,23 @@
 package main
 
 import (
+	"G02-Go-API/bootstrap"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strings"
 )
 
 func main() {
 	//初始化 Gin Engine 实例
-	r := gin.New()
+	router := gin.New()
 
-	// 注册中间件
-	r.Use(gin.Logger(), gin.Recovery())
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
 
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
+	// 运行服务
+	err := router.Run(":3000")
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 
-		// 以 JSON 格式响应
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World!",
-		})
-	})
-
-	//处理 404 请求
-	r.NoRoute(func(c *gin.Context) {
-		// 获取标头信息的 Accept 信息
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// 如果是 HTML 的话
-			c.String(http.StatusNotFound, "页面返回 404")
-		} else {
-			// 默认返回 JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确",
-			})
-		}
-	})
-	// 运行服务, 默认为 8080， 我们指定端口为8000
-	r.Run(":8000")
 }
---
+++ routes/api.go
@@ -0,0 +1,23 @@
+// Package routes 注册路由
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// RegisterAPIRoutes 注册网页相关路由
+func RegisterAPIRoutes(r *gin.Engine) {
+
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放在这里
+	v1 := r.Group("/v1")
+	{
+		// 注册一个路由
+		v1.GET("/", func(c *gin.Context) {
+			// 以 JSON 格式响应
+			c.JSON(http.StatusOK, gin.H{
+				"Hello": "World!",
+			})
+		})
+	}
+}
