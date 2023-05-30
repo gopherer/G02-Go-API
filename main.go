87338c39a62be09cf47f651037ca9739b5ce90fc
gopherer <wuhe092@gmail.com>
2023-05-30 17:12:04 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -2,22 +2,37 @@ package main
 
 import (
 	"G02-Go-API/bootstrap"
+	btsConfig "G02-Go-API/config"
+	"G02-Go-API/pkg/config"
+	"flag"
 	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
-	//初始化 Gin Engine 实例
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
+	// new 一个 Gin Engine 实例
 	router := gin.New()
 
 	// 初始化路由绑定
 	bootstrap.SetupRoute(router)
 
 	// 运行服务
-	err := router.Run(":3000")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
 	}
-
 }
