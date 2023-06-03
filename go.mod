5676fa0a371ea45027af7a0cdc4e4a8d8911341c
gopherer <wuhe092@gmail.com>
2023-06-03 23:46:14 +0800

    验证请求

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.9.0
 	github.com/spf13/cast v1.5.1
 	github.com/spf13/viper v1.15.0
+	github.com/thedevsaddam/govalidator v1.9.10
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/driver/sqlite v1.5.1
 	gorm.io/gorm v1.25.1
--- http/controllers/api/v1/auth/signup_controller.go
+++ http/controllers/api/v1/auth/signup_controller.go
@@ -4,6 +4,7 @@ package auth
 import (
 	v1 "G02-Go-API/http/controllers/api/v1"
 	"G02-Go-API/models/user"
+	"G02-Go-API/requests"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -17,11 +18,8 @@ type SignupController struct {
 // IsPhoneExist 检测手机号是否被注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 
-	// 请求对象
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
-	request := PhoneExistRequest{}
+	// 初始化请求对象
+	request := requests.SignupPhoneExistRequest{}
 
 	// 解析 JSON 请求
 	if err := c.ShouldBindJSON(&request); err != nil {
@@ -35,7 +33,18 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 		return
 	}
 
-	// 检查数据库并返回响应
+	// 表单验证
+	errs := requests.ValidateSignupPhoneExist(&request, c)
+	// errs 返回长度等于零即通过，大于 0 即有错误发生
+	if len(errs) > 0 {
+		// 验证失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
+		return
+	}
+
+	//  检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
 	})
