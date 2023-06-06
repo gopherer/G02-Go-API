08fd4b496c5873946ed574afede985717935b17c
gopherer <wuhe092@gmail.com>
2023-06-06 17:03:26 +0800

    JWT 授权

--- app/models/model.go
+++ app/models/model.go
@@ -1,7 +1,10 @@
 // Package models 模型通用属性和方法
 package models
 
-import "time"
+import (
+	"github.com/spf13/cast"
+	"time"
+)
 
 // BaseModel 模型基类
 type BaseModel struct {
@@ -12,4 +15,9 @@ type BaseModel struct {
 type CommonTimestampsField struct {
 	CreatedAt time.Time `gorm:"column:created_at;index;" json:"created_at,omitempty"`
 	UpdatedAt time.Time `gorm:"column:updated_at;index;" json:"updated_at,omitempty"`
+}
+
+// GetStringID 获取 ID 的字符串格式
+func (a BaseModel) GetStringID() string {
+	return cast.ToString(a.ID)
 }
--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,7 +1,10 @@
 // Package app 应用信息
 package app
 
-import "G02-Go-API/pkg/config"
+import (
+	"G02-Go-API/pkg/config"
+	"time"
+)
 
 func IsLocal() bool {
 	return config.Get("app.env") == "local"
@@ -13,4 +16,10 @@ func IsProduction() bool {
 
 func IsTesting() bool {
 	return config.Get("app.env") == "testing"
+}
+
+// TimenowInTimezone 获取当前时间，支持时区
+func TimenowInTimezone() time.Time {
+	chinaTimezone, _ := time.LoadLocation(config.GetString("app.timezone"))
+	return time.Now().In(chinaTimezone)
 }
