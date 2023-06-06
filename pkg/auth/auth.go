802d733a7370b57908dbb66bb74f5afc3242d1ec
gopherer <wuhe092@gmail.com>
2023-06-06 23:09:56 +0800

    Auth 中间件

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -3,7 +3,9 @@ package auth
 
 import (
 	"G02-Go-API/app/models/user"
+	"G02-Go-API/pkg/logger"
 	"errors"
+	"github.com/gin-gonic/gin"
 )
 
 // Attempt 尝试登录
@@ -28,4 +30,20 @@ func LoginByPhone(phone string) (user.User, error) {
 	}
 
 	return userModel, nil
+}
+
+// CurrentUser 从 gin.context 中获取当前登录用户
+func CurrentUser(c *gin.Context) user.User {
+	userModel, ok := c.MustGet("current_user").(user.User)
+	if !ok {
+		logger.LogIf(errors.New("无法获取用户"))
+		return user.User{}
+	}
+	// db is now a *DB value
+	return userModel
+}
+
+// CurrentUID 从 gin.context 中获取当前登录用户 ID
+func CurrentUID(c *gin.Context) string {
+	return c.GetString("current_user_id")
 }
