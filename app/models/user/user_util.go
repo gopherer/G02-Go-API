4f6362e06c939f99f52da4a38ee25103f4afd39b
gopherer <wuhe092@gmail.com>
2023-06-14 03:04:17 +0800

    用户列表分页

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -1,6 +1,11 @@
 package user
 
-import "G02-Go-API/pkg/database"
+import (
+	"G02-Go-API/pkg/app"
+	"G02-Go-API/pkg/database"
+	"G02-Go-API/pkg/paginator"
+	"github.com/gin-gonic/gin"
+)
 
 // IsEmailExist 判断 Email 已被注册
 func IsEmailExist(email string) bool {
@@ -47,5 +52,17 @@ func GetByEmail(email string) (userModel User) {
 // All 获取所有用户数据
 func All() (users []User) {
 	database.DB.Find(&users)
+	return
+}
+
+// Paginate 分页内容
+func Paginate(c *gin.Context, perPage int) (users []User, paging paginator.Paging) {
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(User{}),
+		&users,
+		app.V1URL(database.TableName(&User{})),
+		perPage,
+	)
 	return
 }
