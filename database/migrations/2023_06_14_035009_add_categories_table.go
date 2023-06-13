0afa82d06b5cf830115f30f209343fffd584d546
gopherer <wuhe092@gmail.com>
2023-06-14 03:52:25 +0800

    分类模型

---
+++ database/migrations/2023_06_14_035009_add_categories_table.go
@@ -0,0 +1,31 @@
+package migrations
+
+import (
+	"G02-Go-API/app/models"
+	"G02-Go-API/pkg/migrate"
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type Category struct {
+		models.BaseModel
+
+		Name        string `gorm:"type:varchar(255);not null;index"`
+		Description string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Category{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Category{})
+	}
+
+	migrate.Add("2023_06_14_035009_add_categories_table", up, down)
+}
