1bdde3a453f90406583fa7d31364515d9bfef976
gopherer <wuhe092@gmail.com>
2023-06-14 13:20:49 +0800

    话题模型和迁移

---
+++ app/models/topic/topic_util.go
@@ -0,0 +1,40 @@
+package topic
+
+import (
+	"G02-Go-API/pkg/app"
+	"G02-Go-API/pkg/database"
+	"G02-Go-API/pkg/paginator"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Get(idstr string) (topic Topic) {
+	database.DB.Where("id", idstr).First(&topic)
+	return
+}
+
+func GetBy(field, value string) (topic Topic) {
+	database.DB.Where("? = ?", field, value).First(&topic)
+	return
+}
+
+func All() (topics []Topic) {
+	database.DB.Find(&topics)
+	return
+}
+
+func IsExist(field, value string) bool {
+	var count int64
+	database.DB.Model(Topic{}).Where("? = ?", field, value).Count(&count)
+	return count > 0
+}
+func Paginate(c *gin.Context, perPage int) (topics []Topic, paging paginator.Paging) {
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(Topic{}),
+		&topics,
+		app.V1URL(database.TableName(&Topic{})),
+		perPage,
+	)
+	return
+}
