db5a9a31149465c43526d19ec25fc689c97d9a9b
gopherer <wuhe092@gmail.com>
2023-06-17 15:00:21 +0800

    编辑个人资料

--- app/models/user/user_model.go
+++ app/models/user/user_model.go
@@ -11,8 +11,13 @@ import (
 type User struct {
 	models.BaseModel
 
-	Name     string `json:"name,omitempty"`
-	Email    string `json:"_"`
+	Name string `json:"name,omitempty"`
+
+	City         string `json:"city,omitempty"`
+	Introduction string `json:"introduction,omitempty"`
+	Avatar       string `json:"avatar,omitempty"`
+
+	Email    string `json:"-"`
 	Phone    string `json:"-"`
 	Password string `json:"-"`
 
